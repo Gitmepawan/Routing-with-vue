524f965bd5a858527ceb2dfacb2246738c21c2f5
pawan pandeya <97752074+Gitmepawan@users.noreply.github.com>
2023-02-15 16:10:22 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,24 +1,43 @@
+//import your modules /components here
+import LoginPage from "./modules/LoginPage.js";
+import HomePage from "./modules/HomePage.js";
+import ErrorPage from "./modules/ErrorPage.js";
 
-// import your modules/components here
-import LoginPage from "./modules/LoginPage.js"
-
+  
 const router = VueRouter.createRouter({
     // 4. Provide the history implementation to use. We are using the hash history for simplicity here.
     history: VueRouter.createWebHashHistory(),
-    routes: [
-      {
-        path: '/',  // the location bar url
-        name: 'login', // name of the route
-        component: LoginPage // component to render
-      }
+    routes: [ //vue will try to match the following routs
+            //and render the approprite component onto the page
+        { 
+        path: '/', //the location bar url
+        name: 'login', // the name  of the route 
+        component: LoginPage // the component to render
+    
+    },
+    { 
+        path: '/home', //the location bar url
+        name: 'home', // the name  of the route 
+        component: HomePage // the component to render
+    },
+    { 
+      path: '/:pathMatch(.*)*', //any other link like pawan-pandeya.ca/user wont work
+      name: 'error', // the name  of the route 
+      component: ErrorPage // the component to render
+  },
     ] // short for `routes: routes`
   })
   
   // 5. Create and mount the root instance.
-  const app = Vue.createApp({})
+  const app = Vue.createApp({
+    methods: { tryRouterPush(){
+      this.$router.push({ name: 'home'});
+    }
+
+    }
+  })
   // Make sure to _use_ the router instance to make the
   // whole app router-aware.
-  app.use(router)
+  app.use(router);
   
-  app.mount('#app')
-  
+  app.mount('#app');
