af572825025ed66991f0939952482f6009e43760
pawan pandeya <97752074+Gitmepawan@users.noreply.github.com>
2023-02-15 16:22:52 -0500

    Update ErrorPage.js

--- js/modules/ErrorPage.js
+++ js/modules/ErrorPage.js
@@ -1,3 +1,12 @@
 export default {
-    template: `<h1>That page doesn't exist!</h1>`
+    template: `<h1>That page doesn't exist!</h1>`, 
+
+    create() {
+        console.log('hit the error route');
+
+        // redirect back to safe route
+        setTimeout(()=>{
+        this.$router.push({ name: 'home'});
+        }, 2500); // wait 2.5 seconds and execute the function
+    }
 }
